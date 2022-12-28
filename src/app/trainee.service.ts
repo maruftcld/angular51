9efe9298b798e02568bf9d22d8b1b2ad2a8ebaa2
Maruf <maruf.tcld@gmail.com>
2022-12-28 11:44:33 +0600

    Update trainee.service.ts

--- src/app/trainee.service.ts
+++ src/app/trainee.service.ts
@@ -10,22 +10,29 @@ export class TraineeService {
   constructor(private http : HttpClient) { }
   
   url : string = "http://localhost:3000/trainee";
+  url2 : string = "http://localhost:8080/student/getAll";
+
+  map : string = "http://localhost:8080/trainee";
 
   saveData(data: Trainee){
-    this.http.post(this.url, data).toPromise();
+    this.http.post(this.map + "/save", data).toPromise();
   }
   deleteData(data: Trainee){
-    this.http.delete(this.url + "/" + data.id).toPromise();
+    this.http.delete(this.map + "/delete/" + data.id).toPromise();
   }
   editData(data: Trainee){
-     return this.http.get<Trainee>(this.url + "/" + data.id);
+     return this.http.get<Trainee>(this.map + "/edit/" + data.id);
   }
   updateData(data: Trainee){
     console.log(data)
     this.http.put(this.url + "/" + data.id, data).toPromise();
  }
   getAll(){
-    return this.http.get<Trainee>(this.url);
+    // console.log("calling.....")
+    // this.http.get(this.map).subscribe((response) => {
+    //   console.log(response);
+    // });
+    return this.http.get<Trainee>(this.map + "/getAll");
   }
 
   
